5b2bcf8d8b0487f1c18a7ed5595f5eb4142cdcb4
Roman Acevedo <roman.acevedo62@gmail.com>
2024-01-23 00:16:53 +0100

    fix: import again

--- packages/renderer/src/file-selection/FileSelectionOverlay.svelte
+++ packages/renderer/src/file-selection/FileSelectionOverlay.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import {getChainsFilenames} from '/ElectronAPIUtils';
+  import {getChainsFilenames} from '/@/ElectronAPIUtils';
 
   export let currentChainsDirectory: string;
   let chainsFilenamesPromise = getChainsFilenames(currentChainsDirectory)
